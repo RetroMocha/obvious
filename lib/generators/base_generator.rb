eca08c09bcd3af051b90cf38a915f5da30e89f6a
Andrew Kalek <andrew.kalek@anlek.com>
2014-01-20 15:15:37 -0800

    Removed unused code.

--- lib/generators/base_generator.rb
+++ lib/generators/base_generator.rb
@@ -13,18 +13,6 @@ module Obvious
         self.app = Obvious::Generators::Application.instance
       end
 
-      def create_directories directories
-        directories.each do |dir|
-          create_directory dir
-        end
-      end
-
-      def create_directory dir="", inside_app = true
-        dir = Pathname.new(dir)
-        dir = Pathname.new(app.dir).join(dir) if inside_app
-        FileUtils.mkdir_p dir
-      end
-
       def camel_case(value)
         value.gsub(/[^\w\d\s\_]+/, "").gsub(/\s/, "_").split('_').map{|e| e.capitalize}.join
       end
--- spec/generators/base_generator_spec.rb
+++ spec/generators/base_generator_spec.rb
@@ -9,17 +9,7 @@ describe Obvious::Generators::BaseGenerator do
   it "generates an instance of an app" do
     expect(subject.app).to eq Obvious::Generators::Application.instance
   end
-  context "create_directory" do
-    let(:dir_name) { Pathname.new("test-dir") }
-    it "generates a directory on the file system with app" do
-      FileUtils.should_receive(:mkdir_p).with(Pathname.new("app").join(dir_name))
-      subject.create_directory(dir_name.to_s)
-    end
-    it "generates a directory on the file system without app folder" do
-      FileUtils.should_receive(:mkdir_p).with(dir_name)
-      subject.create_directory(dir_name.to_s, false)
-    end
-  end
+
   context 'camel_case' do
     it "works correctly" do
       expect(subject.camel_case "create_user").to eq "CreateUser"
