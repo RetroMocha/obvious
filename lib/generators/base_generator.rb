052b172ae2f262fab00a72a720afb7ee2606d20f
Andrew Kalek <andrew.kalek@anlek.com>
2014-01-22 19:27:16 -0800

    Added a file writer (to ensure no files get overwritten)

--- lib/generators/base_generator.rb
+++ lib/generators/base_generator.rb
@@ -1,15 +1,23 @@
 require 'pathname'
 require 'fileutils'
+require 'forwardable'
 require_relative 'helpers/string_ext'
 require_relative 'helpers/exceptions'
 require_relative 'helpers/application'
 require_relative 'helpers/erb_render'
+require_relative 'helpers/file_writer'
 
 module Obvious
   module Generators
     class BaseGenerator
+      include FileWriter
+
+      extend Forwardable
+      def_delegators :app, :jacks, :entities
+
       attr_accessor :app
       attr_reader :argv
+
       def initialize(argv=[])
         @argv = argv
         self.app = Obvious::Generators::Application.instance
--- lib/generators/descriptor_generator.rb
+++ lib/generators/descriptor_generator.rb
@@ -9,35 +9,25 @@ module Obvious
         @name = argv[1]
         raise MissingVariable.new("Unable to generate Descriptor without a name variable") if @name.nil?
         @destination = app.descriptors_dir.join("#{@name}.yml")
-        check_destination!
       end
       def descriptor_name
         @name.camel_case
       end
       def generate
-        result = render(template, action: descriptor_name)
+        result = render(template, {action: descriptor_name})
         puts "Building descriptor #{@destination}"
-        save_template_to_file(result)
+        to_file(result,  @destination)
         puts "Done"
       end
 
       #######
       private
       #######
-      def check_destination!
-        raise DescriptorFileExist.new("'#{@destination}' already exists, remove it before re-running this command") if File.exists? @destination
-      end
       def render(template, variables={})
         ErbRender.render_from_hash(template, variables)
       end
       def template
         @template ||= File.read(Pathname.new(__FILE__).parent.parent.join("obvious", "files", "generators", "descriptor.yml.erb"))
-      end
-      def save_template_to_file(rendering)
-        check_destination!
-        File.open(@destination.expand_path, "wb") {|f|
-          f.write(rendering)
-        }
       end
     end
   end
